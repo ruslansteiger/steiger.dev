15dbd0535850388c359271f011528b8fdd72a150
Ruslan Steiger <steiger.ruslan@gmail.com>
2020-08-15 19:05:11 +0200

    wip

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,16 @@ module.exports = {
 			fontFamily: {
 				sans: ['Inter', ...defaultTheme.fontFamily.sans],
 			},
+			animation: {
+				spin: 'spin 3s linear infinite',
+				wiggle: 'wiggle 1s ease-in-out infinite',
+			},
+			keyframes: {
+				wiggle: {
+					'0%, 100%': { transform: 'rotate(-3deg)' },
+					'50%': { transform: 'rotate(3deg)' },
+				},
+			},
 		},
 	},
 
