20663d5a64ce60243677ec479694f7a68f30c557
Ruslan Steiger <steiger.ruslan@gmail.com>
2021-02-11 09:37:35 +0100

    ✨ Added simple router

---
+++ src/CurriculumVitae.vue
@@ -0,0 +1,5 @@
+<template>
+	<h1 class="bg-pink-700 text-white font-2xl">CV Page</h1>
+</template>
+
+<script setup></script>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,28 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp, h } from 'vue'
 
-createApp(App).mount('#app')
+import NotFound from './NotFound.vue'
+import Home from './Home.vue'
+import CurriculumVitae from './CurriculumVitae.vue'
+
+const routes = {
+	'/': Home,
+	'/cv': CurriculumVitae,
+}
+
+const SimpleRouter = {
+	data: () => ({
+		currentRoute: window.location.pathname,
+	}),
+
+	computed: {
+		CurrentComponent() {
+			return routes[this.currentRoute] || NotFound
+		},
+	},
+
+	render() {
+		return h(this.CurrentComponent)
+	},
+}
+
+createApp(SimpleRouter).mount('#app')
