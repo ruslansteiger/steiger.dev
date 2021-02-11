ca9adffedd3315ebfd6d923b4c17a4a026d14c72
Ruslan Steiger <steiger.ruslan@gmail.com>
2021-02-11 09:09:35 +0100

    🔧 Added basic Vite project with Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,38 +1,11 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
-
 module.exports = {
-	purge: ['./public/index.html'],
-
+	purge: [],
+	darkMode: false, // or 'media' or 'class'
 	theme: {
-		extend: {
-			fontFamily: {
-				sans: ['Inter', ...defaultTheme.fontFamily.sans],
-			},
-			animation: {
-				spin: 'spin 3s linear infinite',
-				wiggle: 'wiggle 1s ease-in-out infinite',
-			},
-			keyframes: {
-				wiggle: {
-					'0%, 100%': { transform: 'rotate(-3deg)' },
-					'50%': { transform: 'rotate(3deg)' },
-				},
-			},
-		},
+		extend: {},
 	},
-
-	variants: [
-		'responsive',
-		'group-hover',
-		'focus-within',
-		'first',
-		'last',
-		'odd',
-		'even',
-		'hover',
-		'focus',
-		'active',
-		'visited',
-		'disabled',
-	],
+	variants: {
+		extend: {},
+	},
+	plugins: [],
 }
